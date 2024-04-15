4442806bcd9dc0443152867feb7a490d5c7146e2
rbarbosa1985 <betoanselmo@gmail.com>
2024-04-15 09:53:03 -0300

    VerifyJWT

--- src/http/routes.ts
+++ src/http/routes.ts
@@ -3,6 +3,7 @@ import { FastifyInstance } from 'fastify'
 import { authenticate } from './controllers/authenticate'
 import { profile } from './controllers/profile'
 import { register } from './controllers/register'
+import { verifyJWT } from './middlewares/verify-jwt'
 
 export async function appRoutes(app: FastifyInstance) {
   // User Routes
@@ -12,5 +13,5 @@ export async function appRoutes(app: FastifyInstance) {
   app.post('/sessions', authenticate)
 
   // ** Authenticated  */
-  app.get('/me', profile)
+  app.get('/me', { onRequest: [verifyJWT] }, profile)
 }
